e54a3c8e8f944c3e5eeae1229ddf75d72c6f3f35
Vincentto13 <pawel.rapkiewicz@gmail.com>
2018-03-05 22:35:13 +0100

    Another set of iprovements into 567 chars

--- snake.cpp
+++ snake.cpp
@@ -1,21 +1,21 @@
 #include <curses.h>
-#include <list>
+#include <queue>
 #include <stdlib.h>
 
 #define L mvaddch
 #define X L(rand()%(N-1)+1,rand()%M,88);mvprintw(0,0,"%d",Q++);
-#define W(y,x){s.push_front({y,x});L(y,x,79);}
+#define W{s.push({y,x});L(y,x,79);}
 
 struct h{int a,b;}; int c,x,y,k,M,N,Q,v,u=-1;
 int main()
 {
-    std::list<h>s;
+    std::queue<h>s;
     auto E=initscr();
     noecho();
     nodelay(E,1);
     getmaxyx(E,N,M);
     x=M/2+5;y=N/2;
-    for(;k++<5;)W(y,x);
+    for(;k++<5;)W;
     X
     for(;;){
         x+=u;
@@ -23,14 +23,14 @@ int main()
         x=x>=M?x=0:x<0?x=M-1:x;
         y=y>=N?y=1:y<1?y=N-1:y;
         c=mvinch(y,x)&127;
-        W(y,x);
+        W;
         if(c==88){X}
         else if(c!=32)
             break;
         else
         {
-            h z=s.back();
-            s.pop_back();
+            h z=s.front();
+            s.pop();
             L(z.a,z.b,32);
         }
         if ((k=getch())>-1)
